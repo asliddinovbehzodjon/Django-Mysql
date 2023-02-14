1e4300077793f9fa1b0c8ab994de41f8893a4aa7
Behzod <behzodtuit@gmail.com>
2023-02-14 17:09:39 +0500

    deploy

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150,verbose_name='Post Title',help_text='Enter post title')
+    description = models.TextField(verbose_name='Post Description',help_text='Enter post description')
+    def __str__(self):
+        return self.title
